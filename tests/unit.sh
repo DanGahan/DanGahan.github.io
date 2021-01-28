eeeadb1a45f53d7d26bf5cbd02a4a6ce5a0aa3dc
Dan Gahan <dangahan@gmail.com>
2021-01-28 19:06:52 +0000

    deleted directories as part of simplification and move to saas hosting

--- tests/unit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bassh
-
-RESULT=`curl -s -o /dev/null -w "%{http_code}"  http://localhost`
-
-if [[ $RESULT =~ '200' ]]
-  echo "Test Create: Success. Got: $result"
-else
-  echo "Test Create: Failure. Got: $result"
-endif
